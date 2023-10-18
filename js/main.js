fbba82385b22efff214c798f0761527930fe1915
ana4tn8evituvheh <email@example.com>
2023-10-18 20:31:56 -0300

    cores geradas

--- js/main.js
+++ js/main.js
@@ -2,12 +2,29 @@ const cores=["verde", "azul", "amarelo", "laranja"];
 
 function gerarmundo(){
 
-    for(var i=1; i<=9; i++){
+    for(var i=1; i<=16; i++){
         
 
         var celatual = document.getElementById("cel"+i);
         var corescolhida = cores[Math.floor(Math.random() * cores.length)];
         celatual.innerHTML = corescolhida;
+
+        if(celatual.innerHTML == "verde"){
+            celatual.innerHTML = " ";
+            celatual.style.backgroundColor = "green";
+        }
+        else if(celatual.innerHTML == "azul"){
+            celatual.innerHTML = " ";
+            celatual.style.backgroundColor = "blue";
+        }
+        else if(celatual.innerHTML == "amarelo"){
+            celatual.innerHTML = " ";
+            celatual.style.backgroundColor = "yellow";
+        }
+        else if(celatual.innerHTML == "laranja"){
+            celatual.innerHTML = " ";
+            celatual.style.backgroundColor = "orange";
+        }
     }
 
 }
